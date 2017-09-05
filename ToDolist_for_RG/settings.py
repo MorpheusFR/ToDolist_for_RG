75d7ac334ef339ddc14868393a5859b8a57993f2
MorpheusFR <Creatorsk@gmail.com>
2017-09-05 16:26:23 +0300

    v0.4 fix

--- ToDolist_for_RG/settings.py
+++ ToDolist_for_RG/settings.py
@@ -121,22 +121,19 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 #STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'static'))
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 
 STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-)
+]
 
 # Settings for django-bootstrap3
-BOOTSTRAP3 = {
-    'include_jquery': True,
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
 
-    }
-#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))#,
-    #'/home/morpheus/PycharmProjects/ToDolist_for_RG/static',)
+
 
 # List of finder classes that know how to find static files in
 # various locations.
