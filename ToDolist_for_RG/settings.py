d9c803ba3bdd630e31c1b15a9151f5ac1ed7b273
MorpheusFR <Creatorsk@gmail.com>
2017-09-05 10:51:10 +0300

    corect setting.py

--- ToDolist_for_RG/settings.py
+++ ToDolist_for_RG/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -121,11 +121,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),
-    '/home/morpheus/PycharmProjects/ToDolist_for_RG/static',)
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"))#,
+    #'/home/morpheus/PycharmProjects/ToDolist_for_RG/static',)
 
 # List of finder classes that know how to find static files in
 # various locations.
